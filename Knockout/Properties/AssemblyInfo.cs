46c88bf3047af1c9f7b1ad00839a205d559f07b9
Erik Källén <erik_kallen@hotmail.com>
2013-01-20 21:04:02 +0100

    Moved files

---
+++ Knockout/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Saltarelle.Knockout")]
+[assembly: AssemblyDescription("Saltarelle Knockout API")]
+[assembly: ScriptAssembly("Knockout")]
+
+[assembly: AssemblyCompany("Erik Källén")]
+[assembly: AssemblyProduct("Saltarelle Knockout.js bindings")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
